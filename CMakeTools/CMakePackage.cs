047941e23c9748fcc6c0d1a3a07dfe97afee1b87
David Golub <golubdr@gmail.com>
2012-04-22 19:39:04 -0400

    Added basic Visual Studio package.

---
+++ CMakeTools/CMakePackage.cs
@@ -0,0 +1,14 @@
+﻿// CMake Tools for Visual Studio
+// Copyright (C) 2012 by David Golub.
+// All rights reserved.
+
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.Shell;
+
+namespace CMakeTools
+{
+    [InstalledProductRegistration("#100", "#101", "1.0")]
+    class CMakePackage : Package
+    {
+    };
+}
