982728178737ba521bb37573d130f9beb092479b
David Golub <golubdr@gmail.com>
2012-04-24 20:45:20 -0400

    Added support for scanning comments and strings.

--- CMakeTools/CMakeScanner.cs
+++ CMakeTools/CMakeScanner.cs
@@ -11,13 +11,80 @@ namespace CMakeTools
     /// </summary>
     class CMakeScanner : IScanner
     {
+        private string _source;
+        private int _offset;
+
         public void SetSource(string source, int offset)
         {
+            _source = source;
+            _offset = offset;
         }
 
         public bool ScanTokenAndProvideInfoAboutIt(TokenInfo tokenInfo, ref int state)
         {
+            if (state == 1 && _offset < _source.Length)
+            {
+                // If the line begins inside a string token, begin by scanning the rest
+                // of the string.
+                ScanString(tokenInfo, ref state, false);
+                return true;
+            }
+            while (_offset < _source.Length)
+            {
+                if (_source[_offset] == '#')
+                {
+                    // Scan a comment token.
+                    tokenInfo.StartIndex = _offset;
+                    tokenInfo.EndIndex = _source.Length - 1;
+                    tokenInfo.Color = TokenColor.Comment;
+                    _offset = _source.Length;
+                    return true;
+                }
+                else if (_source[_offset] == '"')
+                {
+                    // Scan a string token.
+                    ScanString(tokenInfo, ref state, true);
+                    return true;
+                }
+                _offset++;
+            }
             return false;
+        }
+
+        private void ScanString(TokenInfo tokenInfo, ref int state, bool startWithQuote)
+        {
+            tokenInfo.StartIndex = _offset;
+            tokenInfo.Color = TokenColor.String;
+            if (startWithQuote)
+            {
+                // If the string token began with a quotation mark (as opposed to
+                // carrying over from a string on the previous line), advance past it.
+                _offset++;
+            }
+            while (_offset < _source.Length)
+            {
+                if (_source[_offset] == '\\' && _offset != _source.Length - 1)
+                {
+                    // Skip over escape sequences
+                    _offset++;
+                }
+                else if (_source[_offset] == '"')
+                {
+                    // An unescaped quotation mark signals the end of the string.
+                    tokenInfo.EndIndex = _offset;
+                    _offset++;
+                    state = 0;
+                    return;
+                }
+                _offset++;
+            }
+
+            // If we made it to the end of the string without hitting an unescaped
+            // quotation mark, return a string token consisting of the rest of the line
+            // and set the state to carry over onto the next line.
+            _offset = _source.Length;
+            tokenInfo.EndIndex = _source.Length - 1;
+            state = 1;
         }
     }
 }
