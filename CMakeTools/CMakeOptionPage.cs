026da42039277094977956d82bee8f3819eb75e8
David Golub <golubdr@gmail.com>
2012-09-08 10:52:53 -0400

    Added CMakeOptionPage.cs, which was accidentally omitted earlier.

---
+++ CMakeTools/CMakeOptionPage.cs
@@ -0,0 +1,30 @@
+﻿/* ****************************************************************************
+ * 
+ * Copyright (C) 2012 by David Golub.  All rights reserved.
+ * 
+ * This software is subject to the Microsoft Reciprocal License (Ms-RL).
+ * A copy of the license can be found in the License.txt file included
+ * in this distribution.
+ * 
+ * You must not remove this notice, or any other, from this software.
+ * 
+ * **************************************************************************/
+
+using System.ComponentModel;
+using Microsoft.VisualStudio.Shell;
+
+namespace CMakeTools
+{
+    /// <summary>
+    /// Option page to configure CMake Tools for Visual Studio.
+    /// </summary>
+    public class CMakeOptionPage : DialogPage
+    {
+        /// <summary>
+        /// Flags indicating whether to display command names in lowercase.
+        /// </summary>
+        [DisplayName("Commands In Lowercase")]
+        [Description("Display CMake commands with lowercase letters in the IntelliSense list box.")]
+        public bool CommandsLower { get; set; }
+    }
+}
