fc2e48328141942404430c6b7ff5f5310c20526e
David Golub <golubdr@gmail.com>
2012-09-18 20:45:24 -0400

    Added missing copyright header.

--- CMakeTools/EnumToDisplayNameConverter.cs
+++ CMakeTools/EnumToDisplayNameConverter.cs
@@ -1,4 +1,16 @@
-﻿using System;
+﻿/* ****************************************************************************
+ * 
+ * Copyright (C) 2012 by David Golub.  All rights reserved.
+ * 
+ * This software is subject to the Microsoft Reciprocal License (Ms-RL).
+ * A copy of the license can be found in the License.txt file included
+ * in this distribution.
+ * 
+ * You must not remove this notice, or any other, from this software.
+ * 
+ * **************************************************************************/
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Globalization;
