d99d7ecc81df7282b996d1cea79f050c1a021613
David Golub <golubdr@gmail.com>
2013-01-02 20:01:34 -0500

    Updated copyright date for 2013.

--- CMakeTools/Properties/AssemblyInfo.cs
+++ CMakeTools/Properties/AssemblyInfo.cs
@@ -1,6 +1,14 @@
-﻿// CMake Tools for Visual Studio
-// Copyright (C) 2012 by David Golub.
-// All rights reserved.
+﻿/* ****************************************************************************
+ * 
+ * Copyright (C) 2012-2013 by David Golub.  All rights reserved.
+ * 
+ * This software is subject to the Microsoft Reciprocal License (Ms-RL).
+ * A copy of the license can be found in the License.txt file included
+ * in this distribution.
+ * 
+ * You must not remove this notice, or any other, from this software.
+ * 
+ * **************************************************************************/
 
 using System.Reflection;
 using System.Runtime.InteropServices;
@@ -9,7 +17,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("CMakeTools")]
 [assembly: AssemblyDescription("CMake Tools for Visual Studio")]
 [assembly: AssemblyProduct("CMake Tools for Visual Studio")]
-[assembly: AssemblyCopyright("Copyright (C) 2012 by David Golub.  All rights reserved.")]
+[assembly: AssemblyCopyright("Copyright (C) 2012-2013 by David Golub.  All rights reserved.")]
 [assembly: ComVisible(false)]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
